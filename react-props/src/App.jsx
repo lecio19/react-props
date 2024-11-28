51bf712abef49999d0ec3170d0a028970b255551
lecio19 <alex.macri01@gmail.com>
2024-11-28 16:17:22 +0100

    primo commit e Header

---
+++ react-props/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ react-props/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import style from './header.module.css'
+
+export default function Header() {
+  
+    return (
+      <>
+        <div className= {style.title}>IL MIO BLOG</div>
+      </>
+    )
+  }
